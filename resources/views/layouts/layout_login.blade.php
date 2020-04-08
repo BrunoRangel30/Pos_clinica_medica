717103cd82dc8990a09488d0f00d7bc48bbd24a1
BrunoRangel30 <BrunoRangel30>
2020-04-08 01:02:49 -0300

    inicio menu

--- resources/views/layouts/layout_login.blade.php
+++ resources/views/layouts/layout_login.blade.php
@@ -71,50 +71,8 @@
                 </div>
             </div>
         </nav>
-        <div class='pt-1'>
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <div class='container'>
-                    <a class="navbar-brand" href="#"><i class="fas fa-heartbeat"></i></a>
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="navbar-nav mr-auto">
-                            <li class="nav-item dropdown">
-                                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    Cadastrar
-                                    </a>
-                                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="#">Cadastrar Paciente</a>
-                                    <a class="dropdown-item" href="#">Cadastrar Médico</a>
-                                    <a class="dropdown-item" href="#">Cadastrar Recepcionista</a>
-                                </div>
-                            </li>
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                Consultar
-                                </a>
-                                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="#">Lista de Pacientes</a>
-                                <a class="dropdown-item" href="#">Lista de Médicos</a>
-                                <a class="dropdown-item" href="#">Lista de Recepcionista</a>
-                                </div>
-                            </li>
-                            <li class="nav-item">
-                                    <a class="nav-link" href="#">Agendar Consulta</a>
-                            </li>
-                            <li class="nav-item">
-                                <!--<a class="nav-link disabled" href="#">Disabled</a>-->
-                                <a class="nav-link" href="#">Atribruir Perfil</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
-        </div>
-
         <main class="py-4">
-            @yield('content')
+            @yield('content-login')
         </main>
     </div>
 </body>
