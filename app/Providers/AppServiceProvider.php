b835877a6e20bb9aeda5ee69a8e317faa68ce7a4
BrunoRangel30 <BrunoRangel30>
2020-07-10 12:26:19 -0300

    ajuste boot

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,7 +21,7 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot(UrlGenerator $ur)
+    public function boot(UrlGenerator $url)
     {
         //
         if(env('APP_ENV') !== 'local')
