fe8eaba3d19446481f07094f40345033f4afeb9b
BrunoRangel30 <BrunoRangel30>
2020-07-10 12:50:22 -0300

    ajuste boot

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Providers;
-use Illuminate\Contracts\Routing\UrlGenerator;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,12 +21,12 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot(UrlGenerator $url)
+    public function boot()
     {
         //
         if(env('APP_ENV') !== 'local')
         {
-            $url->forceSchema('https');
+            URL::forceScheme('https');
         }
     }
 }
